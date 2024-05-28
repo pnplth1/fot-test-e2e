7e9188e306497ee07596efb1d6296ccbb5a6ad18
pnpl <pnpl@pnpl.local>
2024-05-28 09:46:04 +0700

    test

--- cypress/support/step_definitions/when.ts
+++ cypress/support/step_definitions/when.ts
@@ -1,9 +1,8 @@
-import {When,defineStep as And } from "@badeball/cypress-cucumber-preprocessor";
-import {Locator} from "../page_objects/Locator";
+import {
+  When,
+  defineStep as And,
+} from "@badeball/cypress-cucumber-preprocessor";
 
-When(
-    "I enter {string} into {string}",
-    (value: string,elementName: string) => {
-        cy.getField(elementName).type(value);
-    },
-  );
+When("I enter {string} into {string}", (value: string, elementName: string) => {
+  cy.get(elementName).type(value);
+});
